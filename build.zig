daf640d04b3a3cbbc7e9634cc857ed34c0f50ebd
fjebaker <fergusbkr@gmail.com>
2024-05-29 00:25:50 +0100

    feat: refactor build to use unmodified version of sokol.zig

--- build.zig
+++ build.zig
@@ -2,21 +2,48 @@ const std = @import("std");
 const sokol_build = @import("sokol");
 
 // helper function to build a LazyPath from the emsdk root and provided path components
-fn emSdkLazyPath(b: *std.Build, emsdk: *std.Build.Dependency, subPaths: []const []const u8) std.Build.LazyPath {
-    return emsdk.path(b.pathJoin(subPaths));
+fn emSdkLazyPath(b: *std.Build, emsdk: *std.Build.Dependency) std.Build.LazyPath {
+    return emsdk.path(b.pathJoin(
+        &.{
+            "upstream",
+            "emscripten",
+            "cache",
+            "sysroot",
+            "include",
+        },
+    ));
 }
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const backend = sokol_build.resolveSokolBackend(
+        .auto,
+        target.result,
+    );
+    const backend_cflags = switch (backend) {
+        .d3d11 => "-DSOKOL_D3D11",
+        .metal => "-DSOKOL_METAL",
+        .gl => "-DSOKOL_GLCORE",
+        .gles3 => "-DSOKOL_GLES3",
+        .wgpu => "-DSOKOL_WGPU",
+        else => @panic("unknown sokol backend"),
+    };
+
     const sokol_dep = b.dependency("sokol", .{
         .target = target,
         .optimize = optimize,
     });
     const sokol_mod = sokol_dep.module("sokol");
-    const imgui_dep = sokol_dep.builder.dependency("imgui", .{});
+
+    const imgui_dep = b.dependency("zig_imgui", .{
+        .target = target,
+        .optimize = optimize,
+    });
     const imgui_include = imgui_dep.path("src");
+    const imgui_lib = imgui_dep.artifact("imgui");
+    const implot_lib = imgui_dep.artifact("implot");
 
     const name = "wgui";
     const root_source_file = b.path("src/main.zig");
@@ -30,8 +57,21 @@ pub fn build(b: *std.Build) !void {
             .target = target,
             .optimize = optimize,
         });
+
         example.addIncludePath(sokol_dep.path("src/sokol/c"));
         example.addIncludePath(imgui_include);
+        example.addIncludePath(b.path("src"));
+        example.addCSourceFile(
+            .{ .file = b.path("src/sokol_imgui.c"), .flags = &.{
+                "-DSOKOL_IMGUI_IMPL",
+                backend_cflags,
+            } },
+        );
+        example.linkLibCpp();
+
+        example.linkLibrary(imgui_lib);
+        example.linkLibrary(implot_lib);
+
         example.root_module.addImport("sokol", sokol_mod);
         b.installArtifact(example);
         run = b.addRunArtifact(example);
@@ -45,23 +85,26 @@ pub fn build(b: *std.Build) !void {
             .target = target,
             .optimize = optimize,
         });
+
+        imgui_lib.addSystemIncludePath(emSdkLazyPath(b, emsdk));
+        implot_lib.addSystemIncludePath(emSdkLazyPath(b, emsdk));
+        example.addSystemIncludePath(emSdkLazyPath(b, emsdk));
+
         example.addIncludePath(sokol_dep.path("src/sokol/c"));
         example.addIncludePath(imgui_include);
+        example.addIncludePath(b.path("src"));
+        example.addCSourceFile(
+            .{ .file = b.path("src/sokol_imgui.c"), .flags = &.{
+                "-DSOKOL_IMGUI_IMPL",
+                backend_cflags,
+            } },
+        );
+
+        example.linkLibrary(imgui_lib);
+        example.linkLibrary(implot_lib);
         example.root_module.addImport("sokol", sokol_mod);
 
-        example.addSystemIncludePath(emSdkLazyPath(b, emsdk, &.{
-            "upstream",
-            "emscripten",
-            "cache",
-            "sysroot",
-            "include",
-        }));
-
         const shell_path = sokol_dep.path("src/sokol/web/shell.html").getPath(sokol_dep.builder);
-        const backend = sokol_build.resolveSokolBackend(
-            .auto,
-            target.result,
-        );
 
         const link_step = try sokol_build.emLinkStep(b, .{
             .lib_main = example,
